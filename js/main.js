88fbfc52fcb1b7a0413734d52458f9e15041b765
Koki Fukushima <fksmkuk@gmail.com>
2018-09-26 00:52:43 +0900

    implement "/" focus prototype

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 console.log("now surfing...");
-var linkList = document.getElementsByTagName("a");
+var slashCount = 0;
 $(function () {
-    // "q"を押したらリンクに数字を表示する
+    var linkList = document.getElementsByTagName("a");
+    var textInputList = document.getElementsByTagName("input");
     $("html").keyup(function (e) {
+        // "q"を押したらリンクに数字を表示する
         if (e.which == 81) {
             $.each(linkList, function (index, value) {
                 // aタグの前にspanタグを追加
@@ -16,8 +18,14 @@ $(function () {
                 $(".beforeLink").css("display", "none");
                 linkList[linkNumber].click();
             }, 100);
+        } else if (e.which == 191) {
+            // "/"を押したらinputフォームに飛べる
+            textInputList[slashCount].focus();
+            slashCount++;
+            if (slashCount == textInputList.length) {
+                slashCount = 0;
+            }
         }
     });
 });
 
-// "/"を押したらinputフォームに飛べる
