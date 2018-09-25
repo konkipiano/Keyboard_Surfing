51f693f48cff74cea91162361a41f1ea48751385
Koki Fukushima <fksmkuk@gmail.com>
2018-09-26 00:20:00 +0900

    complete basic functions

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 console.log("now surfing...");
 var linkList = document.getElementsByTagName("a");
-
-// "q"を押したらリンクに数字が表示される
-// 数字を入力したらリンクに飛べる
 $(function () {
+    // "q"を押したらリンクに数字を表示する
     $("html").keyup(function (e) {
         if (e.which == 81) {
+            $.each(linkList, function (index, value) {
+                // aタグの前にspanタグを追加
+                $(linkList[index]).before('<span class="beforeLink">' + index + '</span>');
+            });
+            // spanタグをdisplay
+            $(".beforeLink").css("display", "inline");
+            // 数字を入力したらリンクに飛べる
+            setTimeout(function () {
+                var linkNumber = window.prompt("リンク番号を入力してください");
+                $(".beforeLink").css("display", "none");
+                linkList[linkNumber].click();
+            }, 100);
         }
     });
 });
